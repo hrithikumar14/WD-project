49460159f5b4f79cc4f910769139f7f6d04c38c2
hrithikumar14 <70479763+hrithikumar14@users.noreply.github.com>
2020-08-30 21:12:36 +0530

    JavaScript Codes

---
+++ JAVA SCRIPT/functions.js
@@ -0,0 +1,16 @@
+console.log("THis is Fumctions tutorial in javascript");
+
+// let name = "hrithik";
+function greet(name="hrithik"){
+    
+    console.log(name + " is learning web")
+}
+greet(name);
+ 
+function sum(a,b,c){
+    let d = a + b + c;
+    return d;
+
+}
+let returnval = sum(1,2,3);
+console.log(returnval);
---
+++ JAVA SCRIPT/interaction.js
@@ -0,0 +1,5 @@
+// ALERT n in-browser javascript-does not return anything
+// alert("this is message");
+
+let name = prompt("what is your name ?" ,"type your name");
+console.log(name);
